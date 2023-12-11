1e5984789bdfe7983778533f1f89a1307e7096e9
Jan Obermüller <jan.obermueller@iav.de>
2023-12-11 15:05:28 +0100

    erste Version

---
+++ check.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+from win10toast import ToastNotifier
+
+url = 'https://de.wikipedia.org/wiki/Extensible_Markup_Language'
+
+html_text = requests.get(url).text
+soup = BeautifulSoup(html_text, 'html.parser')
+
+value = soup.css.select_one(".mw-page-title-main").text
+
+toast = ToastNotifier()
+
+toast.show_toast(
+    "site-checker",
+    "value = " + value,
+    duration = 10,
+    icon_path = "at.ico",
+    threaded = True,
+)
